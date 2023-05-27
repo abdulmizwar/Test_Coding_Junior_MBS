b2215a4842fb581252b3d40034bdef90979ad4e5
abdulmizwar <39364787+abdulmizwar@users.noreply.github.com>
2023-05-26 22:22:34 -0700

    penulisan array
    test array

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+
+$nama1 = array(
+    array("firsname" => "Abdul",
+    "lastname" => "Mizwar",
+    "age" => 24),
+    array("firsname" => "iksan",
+    "lastname" => "idris",
+    "age" => 25)
+);
+
+print_r($nama1);
+ 
+?>
